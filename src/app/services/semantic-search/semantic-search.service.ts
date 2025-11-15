34c88370d3dd1c591d93f0cc0c28a4feaed63591
Kgnot <hricaurtem@proton.me>
2025-11-14 23:17:17 -0500

    WebAssembly

--- src/app/services/semantic-search/semantic-search.service.ts
+++ src/app/services/semantic-search/semantic-search.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
+import initWasm, {
+  find_best_items,
+  find_best_items_direct,
+  find_best_items_direct_top_n, find_best_items_top_n
+} from '../../../assets/pkg/semantic_search_wasm';
+import {Product} from '../../models/product';
+
+
 
 @Injectable({ providedIn: 'root' })
 export class SemanticSearchService {
 
-  constructor(private http: HttpClient) {}
+  private wasmReady = initWasm('/assets/pkg/semantic_search_wasm_bg.wasm');
 
-  ///return this.http.post('http://localhost:3000/api/groq', { question });
-  ask(question: string): Observable<any> {
-    return this.http.post('http://localhost:3000/ask', { question });
+
+  async search(query: string, jsonString: string) {
+    await initWasm();
+    return find_best_items(query, jsonString);
   }
+
+  async topN(query:string, items:string, limit:number){ // el items es un json
+    await this.wasmReady;
+    return find_best_items_top_n(query,items,limit);
+  }
+
 }
--- src/app/utils/semantic-search/semantic-search.ts
+++ src/app/utils/semantic-search/semantic-search.ts
@@ -1,31 +1,37 @@
 import { Component } from '@angular/core';
-import { SemanticSearchService } from '../../services/semantic-search/semantic-search.service';
 import { FormsModule } from '@angular/forms';
+import {SemanticSearchService} from '../../services/semantic-search/semantic-search.service';
+import data from '../../../assets/data/inventory.json'
+import {JsonPipe} from '@angular/common';
+
+
 
 @Component({
   selector: 'app-semantic-search',
   templateUrl: './semantic-search.html',
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   styleUrl: 'semantic-search.scss'
 })
 export class SemanticSearchComponent {
 
-  question = "";
-  answer: string = "";
+  result: any = null;
 
-  constructor(private search: SemanticSearchService) {}
-
-  async submit() {
-    this.answer = "Cargando...";
-
-    this.search.ask(this.question).subscribe({
-      next: (res: any) => {
-        this.answer = res.answer;
-      },
-      error: (err) => {
-        console.error(err);
-        this.answer = "Hubo un error en el servidor.";
-      }
-    });
+  constructor(private semantic: SemanticSearchService) {
   }
+
+  test(){
+    this.semantic.search('Quiero un vector system pro', JSON.stringify(data))
+      .then(r => {
+        this.result = r.map((m: Map<string, any>) => Object.fromEntries(m));
+      });
+
+  }
+
+  testTopN() {
+    this.semantic.topN('quiro una suspencion lineal negra ', JSON.stringify(data), 5)
+      .then(r => this.result = r.map((m:Map<string,any>) => Object.fromEntries(m)))
+      .catch(e => console.error("Error WASM topN:", e));
+  }
+
+
 }
