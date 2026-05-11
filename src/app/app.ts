c9415efec60ecb080ccb86aefe97072f673eb2b3
Kgnot <hricaurtem@proton.me>
2026-05-10 22:18:38 -0500

    Add cart functionality with toast notification for product addition

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,33 @@
-import {Component, OnInit, signal} from '@angular/core';
-import {RouterOutlet, RouterModule, Router, NavigationEnd} from '@angular/router';
-import {Header} from './utils/header/header';
-import {FooterComponent} from './utils/footer/footer.component';
-import {SemanticSearchComponent} from './utils/semantic-search/semantic-search';
-import {filter} from 'rxjs';
-
+import { Component, signal, inject, PLATFORM_ID } from '@angular/core';
+import { RouterOutlet, RouterModule, Router, NavigationEnd } from '@angular/router';
+import { Header } from './utils/header/header';
+import { FooterComponent } from './utils/footer/footer.component';
+import { SemanticSearchComponent } from './utils/semantic-search/semantic-search';
+import { filter } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { isPlatformBrowser } from '@angular/common';
+import {CartToastAddProductComponent} from './utils/cart-toast-add-product.component/cart-toast-add-product.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, Header, FooterComponent, SemanticSearchComponent],
+  imports: [RouterOutlet, RouterModule, Header, FooterComponent, SemanticSearchComponent,CartToastAddProductComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
+export class App {
   protected readonly title = signal('LeaderLedAngular');
 
-  constructor(private router: Router) {
-  }
+  private router = inject(Router);
+  private platformId = inject(PLATFORM_ID);
 
-  ngOnInit() {
+  constructor() {
     this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
+      filter(event => event instanceof NavigationEnd),
+      takeUntilDestroyed()
     ).subscribe(() => {
-      window.scrollTo(0, 0);
-    })
+      if (isPlatformBrowser(this.platformId)) {
+        window.scrollTo({ top: 0, behavior: 'instant' });
+      }
+    });
   }
-
 }
